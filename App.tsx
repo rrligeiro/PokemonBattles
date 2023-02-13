cca3d403985d363b5c1c2ecbc338c51b790c5ea0
Rodrigo Ligeiro <rrligeiro@gmail.com>
2023-02-13 15:24:03 -0300

    Final adjustments

--- App.tsx
+++ App.tsx
@@ -4,34 +4,16 @@ import { ThemeProvider } from "styled-components";
 import theme from "./src/theme/theme";
 import {
   useFonts,
-  SourceSansPro_200ExtraLight,
-  SourceSansPro_200ExtraLight_Italic,
-  SourceSansPro_300Light,
-  SourceSansPro_300Light_Italic,
   SourceSansPro_400Regular,
-  SourceSansPro_400Regular_Italic,
-  SourceSansPro_600SemiBold,
-  SourceSansPro_600SemiBold_Italic,
   SourceSansPro_700Bold,
-  SourceSansPro_700Bold_Italic,
   SourceSansPro_900Black,
-  SourceSansPro_900Black_Italic,
 } from "@expo-google-fonts/source-sans-pro";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    SourceSansPro_200ExtraLight,
-    SourceSansPro_200ExtraLight_Italic,
-    SourceSansPro_300Light,
-    SourceSansPro_300Light_Italic,
     SourceSansPro_400Regular,
-    SourceSansPro_400Regular_Italic,
-    SourceSansPro_600SemiBold,
-    SourceSansPro_600SemiBold_Italic,
     SourceSansPro_700Bold,
-    SourceSansPro_700Bold_Italic,
     SourceSansPro_900Black,
-    SourceSansPro_900Black_Italic,
   });
 
   if (!fontsLoaded) {
